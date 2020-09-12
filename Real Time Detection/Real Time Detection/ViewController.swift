78ce7c099124dee036516191f588a0cc94667b3b
Ebo Sompa Dennis <esdennis01@gmail.com>
2020-09-12 02:10:01 -0400

    Init iOS commit

---
+++ Real Time Detection/Real Time Detection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Real Time Detection
+//
+//  Created by Ebo Sompa Dennis on 9/12/20.
+//  Copyright © 2020 Ebo Sompa Dennis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
